99e9eb41b1332db2dd6dfd918f42e2723c68d694
图像技术集团 <77779228+ImageVision@users.noreply.github.com>
2023-04-29 20:34:19 +0800

    Add files via upload

---
+++ Dev-C++テストエンジニアリング/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include"FunctionDeclaration.h" 
+using namespace std;
+
+int main() 
+{
+	Gray("24.bmp","output.bmp"); 
+	return 0;
+}
---
+++ Visual Studio 2019テストエンジニアリング/测试.cpp
@@ -0,0 +1,9 @@
+﻿#include "ImageVision.h"
+#include <stdio.h>
+
+int main()
+{
+	char input[20] = "24.bmp";
+	char output[20]="output.bmp";
+	Gray(input,output);
+}
